68e3eacdca8ee9a0b08dabddfcdce291803d5354
HajarElShorafaa <99567300+HajarElShorafaa@users.noreply.github.com>
2025-05-17 15:30:59 +0300

    candidate update

--- cypress/e2e/step_definitions/candidateSteps.js
+++ cypress/e2e/step_definitions/candidateSteps.js
@@ -1,14 +1,10 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-import LoginPage from '../pages/LoginPage';
-import CandidatePage from '../pages/candidatePage';
-
-const candidate = new CandidatePage();
 
 let candidateId;
 let cookies={};
 
 Given("I navigate to the candidate page", () => {
-  candidate.visit();
+  cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates")
   cy.getCookies().then((cookiesList) => {
   cookiesList.forEach((cookie) => {
     cy.log(`${cookie.name}: ${cookie.value}`);
@@ -16,21 +12,20 @@ Given("I navigate to the candidate page", () => {
   cy.getCookie('orangehrm').then((cookie) => {
   cookies["orangehrm"] = cookie.value;
   });
-  });
+});
 });
 
 
 When("I add a new candidate using API", () => {
   //cy.addCandidateAPI();
-const uniqueEmail = `john.${Date.now()}@example.com`;
+  const uniqueEmail = `john.${Date.now()}@example.com`;
 
   cy.request({
     method: "POST",
-    url: "/index.php/api/v2/recruitment/candidates",
+    url: "/web/index.php/api/v2/recruitment/candidates",
     headers: {
       Cookie: `orangehrm=${cookies["orangehrm"]}`,
       "X-Requested-With": "XMLHttpRequest",
-      "Content-Type": "application/json"
     },
     body: {
       firstName: "John",
@@ -50,8 +45,8 @@ When("I delete the candidate using API", () => {
   //const candidateId = Cypress.env("candidateId");
   //cy.deleteCandidateAPI(candidateId);
   cy.request({
-    method: "POST",
-    url: "/index.php/api/v2/recruitment/candidates/bulk-delete",
+    method: "DELETE",
+    url: "/web/index.php/api/v2/recruitment/candidates",
     headers: {
       Cookie: `orangehrm=${cookies["orangehrm"]}`,
       "X-Requested-With": "XMLHttpRequest",
